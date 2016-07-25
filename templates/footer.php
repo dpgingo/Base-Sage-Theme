280ca6e275bcc9ef42f14afdf689d1a9dc9c7a0f
Darragh Power <dpgingo@gmail.com>
2016-07-25 12:32:16 +0100

    Initial commit

---
+++ templates/footer.php
@@ -0,0 +1,54 @@
+    <footer class="footer-distributed container-fluid">
+               <div class="container">
+                   <div class="footer-left">
+
+				<img class="img-responsive" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=350%C3%97150&w=350&h=150"/>
+
+				<p class="footer-links">
+					
+<?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+				</p>
+
+				<p class="footer-company-name">Company Name &copy; 2015</p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>123 Fake Street</span> Earth, The Universe</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>051 343213</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:support@company.com">support@company.com</a></p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>About the company</span>
+					Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
+				</p>
+
+				<div class="footer-icons">
+
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-github"></i></a>
+
+				</div>
+
+			</div>
+               </div>
+                
+
+		</footer>
---
+++ templates/header.php
@@ -0,0 +1,26 @@
+<?php
+  // This file assumes that you have included the nav walker from https://github.com/twittem/wp-bootstrap-navwalker
+  // somewhere in your theme.
+?>
+
+<header class="banner navbar navbar-inverse navbar-static-top" role="banner">
+    <div class="container">
+        <div class="navbar-header" id="main-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><img class="img-responsive" src="https://placeholdit.imgix.net/~text?txtsize=33&amp;txt=350%C3%97150&amp;w=350&amp;h=150"></a>
+        </div>
+
+        <nav class="collapse navbar-collapse" role="navigation">
+          <?php
+          if (has_nav_menu('primary_navigation')) :
+            wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
+          endif;
+          ?>
+        </nav>
+    </div>
+</header>
